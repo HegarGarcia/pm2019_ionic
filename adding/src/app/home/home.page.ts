b5560c031b7c73bde1cec8bb921de2f3f082b75a
Hegar Garcia <hegargarcia@gmail.com>
2019-11-14 19:02:24 -0600

    Adding adding program code

--- adding/src/app/home/home.page.ts
+++ adding/src/app/home/home.page.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public firstValue: string;
+  public secondValue: string;
+  public result: string;
 
   constructor() {}
 
+  private add() {
+    if (!!this.firstValue && !!this.secondValue) {
+      this.result = (
+        parseFloat(this.firstValue) + parseFloat(this.secondValue)
+      ).toString();
+    }
+  }
 }
