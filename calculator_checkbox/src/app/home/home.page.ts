293c6caead1e72a95840f546cc52a16368c7cf45
Hegar Garcia <hegargarcia@gmail.com>
2019-11-14 19:10:45 -0600

    Adding calculator with checkboxes code

--- calculator_checkbox/src/app/home/home.page.ts
+++ calculator_checkbox/src/app/home/home.page.ts
@@ -3,10 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public result: string;
+  public firstValue: string;
+  public secondValue: string;
+  public doAddition: boolean;
+  public doSubstraction: boolean;
 
   constructor() {}
 
+  public calculate() {
+    if (!!this.firstValue && !!this.secondValue) {
+      this.result = '';
+      const firstValue = parseFloat(this.firstValue);
+      const secondValue = parseFloat(this.secondValue);
+
+      if (this.doAddition) {
+        this.result += `${firstValue} + ${secondValue} = ${firstValue +
+          secondValue}\n`;
+      }
+
+      if (this.doSubstraction) {
+        this.result += `${firstValue} - ${secondValue} = ${firstValue -
+          secondValue}`;
+      }
+    } else {
+      this.result = 'Error in the data';
+    }
+  }
 }
