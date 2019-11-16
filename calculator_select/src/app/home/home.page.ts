1fc26ae328bf26be054f026fdf78a42bc9209317
Hegar Garcia <hegargarcia@gmail.com>
2019-11-16 11:38:36 -0600

    Calculator select

--- calculator_select/src/app/home/home.page.ts
+++ calculator_select/src/app/home/home.page.ts
@@ -3,10 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public result: string;
+  public firstValue: string;
+  public secondaValue: string;
+  public operation: string;
 
   constructor() {}
 
+  public calculate() {
+    if (this.firstValue && this.secondaValue) {
+      const firstValue = parseFloat(this.firstValue);
+      const secondaValue = parseFloat(this.secondaValue);
+
+      switch (this.operation) {
+        case 'addition':
+          this.result = `${firstValue} + ${secondaValue} = ${firstValue +
+            secondaValue}`;
+          break;
+        case 'substraction':
+          this.result = `${firstValue} - ${secondaValue} = ${firstValue -
+            secondaValue}`;
+          break;
+        case 'multiplication':
+          this.result = `${firstValue} x ${secondaValue} = ${firstValue *
+            secondaValue}`;
+          break;
+        default:
+          this.result = 'Error!';
+          break;
+      }
+    }
+  }
 }
