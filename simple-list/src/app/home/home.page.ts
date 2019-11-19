3e0f1d84fc0851ea103ffa713ebc409dc47afb5b
Hegar Garcia <hegargarcia@gmail.com>
2019-11-18 19:41:15 -0600

    Adding simple list project

--- simple-list/src/app/home/home.page.ts
+++ simple-list/src/app/home/home.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public gameName: string;
+  public gameList: string[] = [];
 
   constructor() {}
 
+  public add() {
+    if (this.gameName) {
+      this.gameList.push(this.gameName);
+      this.gameName = '';
+    }
+  }
 }
