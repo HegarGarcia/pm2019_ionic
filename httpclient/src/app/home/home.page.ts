a324b57ef3d95b7d99c84768217fdd699aca73e5
Hegar Garcia <hegargarcia@gmail.com>
2019-11-18 19:49:41 -0600

    Adding http client project

--- httpclient/src/app/home/home.page.ts
+++ httpclient/src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public records: any[];
 
-  constructor() {}
+  constructor(public http: HttpClient) {}
 
+  ngOnInit() {
+    const uri = 'https://www.w3schools.com/angular/customers.php';
+
+    this.http.get(uri).subscribe((data: any) => {
+      this.records = data.records;
+    });
+  }
 }
